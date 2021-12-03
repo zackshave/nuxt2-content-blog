a52e6675c3a069d117d26a5fc8e8824f3a08ec6a
Zack Shave <zackshave@gmail.com>
2021-12-03 13:33:57 -0400

    fix(blog): added index.vue

---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="container">
+    <div v-if="page.image && page.title" class="grid">
+      <nuxt-picture
+        v-if="page.image"
+        provider="cloudinary" 
+        :src="page.image" 
+        fit="cropping" 
+        width="600" 
+        height="600" 
+        class="hero"
+        :modifiers="{ gravity: 'face' }"
+      />
+      <div class="article__heading">
+        <h1 v-if="page.title">{{ page.title }}</h1>
+        <p>{{ page.description }}</p>
+      </div>
+    </div>
+    <nuxt-content :articles="articles" :document="page" />
+  </section>
+</template>
+
+<script>
+export default {
+  async asyncData({$content, params}) {
+    const page = await $content("index").fetch();
+    const articles = await $content('articles')
+        .only(['title', 'description', 'image', 'slug'])
+        .sortBy('createdAt', 'asc')
+        .fetch()
+    return { 
+      page: { 
+        ...page, 
+        articles 
+      }
+    }
+  },
+  data() {
+    return {
+      articles: null,
+    }
+  }
+}
+</script>
